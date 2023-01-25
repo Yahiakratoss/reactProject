d9679d0679261e63ba580cc94031aa3789adfc9b
elasri.yahia <elasriyahia50@gmail.com>
2023-01-25 20:57:15 +0000

    Updates

---
+++ precache-manifest.087850f8e5bce3ba3f7b6946eb0a560a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fea8d361de541887433d",
+    "url": "/reactProject/static/css/main.b9776aa3.chunk.css"
+  },
+  {
+    "revision": "fea8d361de541887433d",
+    "url": "/reactProject/static/js/main.fea8d361.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/reactProject/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "88b149f373b0f14c3f5a",
+    "url": "/reactProject/static/js/2.88b149f3.chunk.js"
+  },
+  {
+    "revision": "ccb72ee964ab4df16386",
+    "url": "/reactProject/static/js/runtime~main.ccb72ee9.js"
+  },
+  {
+    "revision": "9b9e78692d7010b71f4b521d6c66db83",
+    "url": "/reactProject/static/media/sunset.9b9e7869.jpg"
+  },
+  {
+    "revision": "9d8921deb0f5e5682fb120eb0da60df4",
+    "url": "/reactProject/index.html"
+  }
+];
